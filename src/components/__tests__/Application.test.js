b36622a9e7e6ca3d6f5ae0b82bddf6760ae5a5fd
Rishad Alam <rishad.s.a@gmail.com>
2023-07-20 04:01:34 -0400

    setting up more tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { render, act, waitForElement, fireEvent } from "@testing-library/react";
+import { render, act, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
 import Application from "components/Application";
 import axios from "__mocks__/axios";
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Appointment", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { container } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+    const { container } = render(<Application />);
+    console.log(prettyDOM(container));
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });
