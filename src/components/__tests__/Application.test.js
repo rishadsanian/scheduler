bcd905d6ee2f7451ff984d3d0284c3d586324576
Rishad Alam <rishad.s.a@gmail.com>
2023-07-19 20:40:35 -0400

    made mock get module and created test to stimulate get request

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { render, act } from "@testing-library/react";
+import { render, act, waitForElement, fireEvent } from "@testing-library/react";
 import Application from "components/Application";
+import axios from "__mocks__/axios";
 
-it.skip("renders without crashing", () => {
-  act(() => {
-    render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
-
 });
--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -3,9 +3,9 @@ import { render } from "@testing-library/react";
 import Button from "components/Button";
 
 
-it("renders without crashing", () => {
-  render(<Button />);
-});
+// it("renders without crashing", () => {
+//   render(<Button />);
+// });
 
 it("renders its `children` prop as text", () => {
   const { getByText } = render(<Button>Default</Button>);
