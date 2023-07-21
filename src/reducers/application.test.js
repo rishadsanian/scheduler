4b6ee88d29ca3da151e4c9bdff852ea34c54cccc
Rishad Alam <rishad.s.a@gmail.com>
2023-07-20 22:38:21 -0400

    reducer test

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -2,6 +2,7 @@ import reducer from "reducers/application";
 
 describe("Application Reducer", () => {
   it("thows an error with an unsupported type", () => {
-    expect(() => reducer({}, { type: null })).toThrow();
+    expect(() => reducer({}, { type: null })).toThrow(      /tried to reduce with unsupported action type/i
+    );
   });
 });
